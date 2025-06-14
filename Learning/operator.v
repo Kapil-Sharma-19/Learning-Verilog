2a1117850f8afbdd89bb571975a6620581482ab0
Kapil Sharma <kapilsharma01021980@gmail.com>
2025-06-14 11:56:14 +0530

    first commit

---
+++ Learning/operator.v
@@ -0,0 +1,18 @@
+// Operators
+
+// Unary Operators -> 
+// & -> and
+// | -> or
+// ~ -> not
+// ~& -> nand
+// ~| -> nor
+// ^ -> xor
+// ~^ -> xnor
+
+// Binary Operators ->
+// & -> and
+// | -> or
+// ~& -> nand
+// ~| -> nor
+// ^ -> xor
+// ~^ -> xnor
